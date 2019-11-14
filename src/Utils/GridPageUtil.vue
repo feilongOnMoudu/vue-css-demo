5b2c7cf21c5ed9f9d8ea2f37de58b5cd118beead
songfeilong <songfeilong@cifm.com>
2019-11-14 17:39:45 +0800

    1

--- src/Utils/GridPageUtil.vue
+++ src/Utils/GridPageUtil.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2019-11-13 13:13:16
- * @LastEditTime: 2019-11-14 17:06:39
+ * @LastEditTime: 2019-11-14 17:38:58
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /vue-css-demo/src/Utils/GridPageUtil.vue
@@ -20,10 +20,10 @@
 </template>
 
 <script>
-
-const GridsList = () => import('../components/GridsList.vue');
-const GridsList_1 = () => import('../components/GridsList_1.vue');
-const GridsList_2 = () => import('../components/GridsList_2.vue');
+// import(/* webpackChunkName: "GridsList" */ '../components/GridsList.vue'); 可以把声明的组件打包在GridsList-GridsList.2fdd8c6f.js的js文件中
+const GridsList = () => import(/* webpackChunkName: "GridsList" */ '../components/GridsList.vue');
+const GridsList_1 = () => import(/* webpackChunkName: "GridsList" */ '../components/GridsList_1.vue');
+const GridsList_2 = () => import(/* webpackChunkName: "GridsList" */ '../components/GridsList_2.vue');
 
 export default {
   components: {
