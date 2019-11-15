daf2b27912463b50dacab578c23701ede153b8d2
songfeilong <songfeilong@cifm.com>
2019-11-15 16:37:08 +0800

    增加轮播

---
+++ src/components/Swiper.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="swiper" :style="setCarouselSize()">
+    <div class="window">
+      <ul class="container" ref="imagesWrapper">
+        <li v-for="(e,i) in data" :key="i">
+          <img :src="e.imgUrl" :alt="e.title" />
+        </li>
+      </ul>
+      <ol class="point-wrap">
+        <li :class="{active:i==currentIndex}" v-for="(e,i) in data" :key="i"></li>
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    data:{
+      type:Array,
+      required:true,
+      default: null
+    }
+  },
+  watch: {
+    distance(val) {
+      this.distance = val;
+    }
+  },
+  mounted() {
+    this.windowChange();
+    this.init();
+    
+  },
+  methods: {
+    setCarouselSize() {
+      return "height: " + (288 / 714) * document.body.clientWidth + "px";
+    },
+    // 窗口变化 重新初始化
+    windowChange() {
+      const that = this;
+      window.onresize = () => {
+        return (() => {
+          window.screenWidth = document.body.clientWidth;
+          that.distance = window.screenWidth;
+          this.init();
+        })();
+      };
+    },
+    init() {
+      let wrappers = this.$refs.imagesWrapper;
+      let children = wrappers.children;
+      let total = this.data.length;
+      if (total <= 0) return;
+      // 纯js操作 只需要先将三张图片位置确定好
+      // 最左边按道理说是没有图片的 但是为了无限滚动效果 这里将其置为最后一张
+      let left = total - 1;
+      let center = 0;
+      let right = 1;
+      // 初始化的时候将li左移动了一个屏幕宽度，就是为了防止叠加的元素挡住要显示的图
+      // 现在第一张显示的图片实际是最后一张 3张轮播起来就行了 没必要对每个图片进行位置计算
+      // left处于最左边的位置 不显示
+      children[left].style.transform = "translateX(" + -this.distance + "px)";
+      // center处于中间位置 显示
+      children[center].style.transform = "translateX(" + 0 + "px)";
+      // right处于右边 不显示
+      children[right].style.transform = "translateX(" + this.distance + "px)";
+      this.sliderItem = children;
+      this.play();
+    },
+    next() {
+      this.currentIndex++;
+      // 边界判断
+      if (this.currentIndex > this.data.length - 1) {
+        this.currentIndex = 0;
+      }
+      // center 为显示的图片
+      let center = this.currentIndex;
+      // 左边的 如果为负数 就取最后一张图片下标
+      let left = center - 1 < 0 ? this.data.length - 1 : center - 1;
+      // 右边的 如果超过了最大图片数量 取第一张图片下标
+      let right = center + 1 == this.data.length ? 0 : center + 1;
+      let children = this.sliderItem;
+      // 给元素添加过渡
+      children[center].style.transition = "transform .5s";
+      children[left].style.transition = "transform .5s";
+      // 右边的图片是替补图片，不需要走过渡
+      children[right].style.transition = "none";
+      // 3张图片同时移动
+      children[left].style.transform = "translateX(" + -this.distance + "px)";
+      children[center].style.transform = "translateX(0px)";
+      children[right].style.transform = "translateX(" + this.distance + "px)";
+    },
+    play() {
+      if (this.timer) {
+        window.clearInterval(this.timer);
+        this.timer = null;
+      }
+      this.timer = window.setInterval(() => {
+        this.next();
+      }, this.interval);
+    }
+  },
+  data() {
+    return {
+      sliderItem: {},
+      swiperHeight: "height: 200px",
+      timer: null,
+      currentIndex: 0,
+      speed: 375,
+      distance:
+        window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth
+    };
+  },
+  computed: {
+    interval() {
+      return 3 * 1000;
+    }
+  }
+};
+</script>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+  list-style-type: none;
+}
+img {
+  width: 100%;
+  height:100%;
+}
+.container {
+  position: relative;
+  min-height: 100%;
+}
+.container li {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: translateX(100%);
+}
+.window {
+  position: relative;
+  height: 100%;
+  margin: 0 auto;
+  overflow: hidden;
+}
+.swiper {
+  width: 100%;
+  background-color: rgb(176, 179, 176);
+  text-align: center;
+  box-sizing: border-box;
+}
+ul,
+ol {
+  list-style: none;
+}
+.point-wrap {
+  text-align: center;
+  font-size: 0;
+  position: absolute;
+  left: 0;
+  bottom: 10px;
+  width: 100%;
+}
+.point-wrap li {
+  width: 7px;
+  height: 7px;
+  border: 1px solid #fff;
+  border-radius: 50%;
+  display: inline-block;
+  margin: 0 3px;
+}
+.point-wrap li.active {
+  background-color: #fff;
+}
+</style>
--- src/pages/NewHome.vue
+++ src/pages/NewHome.vue
@@ -1,76 +1,112 @@
 <template>
   <div>
-    <GridPageUtil :data="item" />
+    <Swiper :data="data.swiperItem" />
+    <GridPageUtil :data="data.gridItem" />
   </div>
 </template>
 
 <script>
+const GridPageUtil = () => import("../Utils/GridPageUtil.vue");
 
-const GridPageUtil = () => import('../Utils/GridPageUtil.vue');
+const Swiper = () => import("../components/Swiper.vue");
 
 export default {
   components: {
-    GridPageUtil
+    GridPageUtil,
+    Swiper
   },
   data() {
     return {
-      item: [
-        {
-          type: "list_1",
-          data: [
-            {
-              title: "你好1_1",
-              color: "#44c522",
-              list: ["1", "2", "3", "4", "5", "6"]
-            },
-            {
-              title: "你好2_2",
-              color: "#3d98ff",
-              list: ["1", "2", "3", "4", "5", "6"]
-            }
-          ]
-        },
-        {
-          type: "list_0",
-          data: [
-            {
-              title: "你好1",
-              color: "#44c522",
-              list: ["1", "2", "3", "4"]
-            },
-            {
-              title: "你好2",
-              color: "#3d98ff",
-              list: ["1", "2", "3", "4"]
-            },
-            {
-              title: "你好3",
-              color: "#fc9720",
-              list: ["1", "2", "3", "4"]
-            },
-            {
-              title: "你好4",
-              color: "#ff697a",
-              list: ["1", "2", "3", "4"]
-            }
-          ]
-        },
-        {
-          type: "list_2",
-          data: [
-            {
-              title: "你好3_1",
-              color: "#44c522",
-              list: ["1", "2", "3", "4", "5"]
-            },
-            {
-              title: "你好3_2",
-              color: "#3d98ff",
-              list: ["1", "2", "3", "4"]
-            }
-          ]
-        }
-      ]
+      data: {
+        swiperItem: [
+          {
+            imgUrl: "https://m.cifm.com/src/theme/img/3.5/index-banner0.png"
+          },
+          {
+            imgUrl: "https://m.cifm.com/src/theme/img/3.5/index-banner1.png"
+          },
+          {
+            imgUrl: "https://m.cifm.com/src/theme/img/3.5/index-banner2.png"
+          },
+          {
+            imgUrl: "https://m.cifm.com/src/theme/img/3.5/index-banner3.png"
+          }
+        ],
+        gridItem: [
+          {
+            type: "swiper",
+            data: [
+              {
+                imgUrl: "https://m.cifm.com/src/theme/img/3.5/index-banner0.png"
+              },
+              {
+                imgUrl: "https://m.cifm.com/src/theme/img/3.5/index-banner1.png"
+              },
+              {
+                imgUrl: "https://m.cifm.com/src/theme/img/3.5/index-banner2.png"
+              },
+              {
+                imgUrl: "https://m.cifm.com/src/theme/img/3.5/index-banner3.png"
+              }
+            ]
+          },
+          {
+            type: "list_1",
+            data: [
+              {
+                title: "你好1_1",
+                color: "#44c522",
+                list: ["1", "2", "3", "4", "5", "6"]
+              },
+              {
+                title: "你好2_2",
+                color: "#3d98ff",
+                list: ["1", "2", "3", "4", "5", "6"]
+              }
+            ]
+          },
+          {
+            type: "list_0",
+            data: [
+              {
+                title: "你好1",
+                color: "#44c522",
+                list: ["1", "2", "3", "4"]
+              },
+              {
+                title: "你好2",
+                color: "#3d98ff",
+                list: ["1", "2", "3", "4"]
+              },
+              {
+                title: "你好3",
+                color: "#fc9720",
+                list: ["1", "2", "3", "4"]
+              },
+              {
+                title: "你好4",
+                color: "#ff697a",
+                list: ["1", "2", "3", "4"]
+              }
+            ]
+          },
+          {
+            type: "list_2",
+            data: [
+              {
+                title: "你好3_1",
+                color: "#44c522",
+                list: ["1", "2", "3", "4", "5"]
+              },
+              {
+                title: "你好3_2",
+                color: "#3d98ff",
+                list: ["1", "2", "3", "4"]
+              }
+            ]
+          }
+        ]
+      }
     };
   },
   methods: {}
